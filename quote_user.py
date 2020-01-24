87f2eccf017de32211c8213617b33c136643a59a
Ian Rones <ronesp.85@gmail.com>
2020-01-24 13:05:13 -0800

    [add] quote_user.py

---
+++ quote_user.py
@@ -0,0 +1,11 @@
+from python_quote import random_python_quote
+
+quotes = (
+    "It's just a flesh wound",
+    "He's not the Messiah. He's a very naughty boy!",
+    "THIS IS AN EX-PARROT!!"
+)
+
+if __name__ == '__main__':
+    quote = random_python_quote()
+    print(quote)
